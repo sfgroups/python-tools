215b937958130411993719f5c0d8cf62188969ec
software groups <admin@sfgroups.com>
2022-07-21 22:42:19 -0400

    added  mongodb sample code

---
+++ lib/db/connect_mongodb.py
@@ -0,0 +1,19 @@
+from dotenv import find_dotenv, dotenv_values
+from requests.structures import CaseInsensitiveDict
+import pymongo
+
+
+def connect_mongodb():
+    config = CaseInsensitiveDict(dotenv_values(find_dotenv()))
+    connection_string = config["MONGO_CONNECT"]
+    client = pymongo.MongoClient(connection_string)
+    try:
+        client.server_info()  # validate connection string
+    except pymongo.errors.ServerSelectionTimeoutError:
+        raise TimeoutError("Invalid API for MongoDB connection string or timed out when attempting to connect")
+
+    return client
+
+
+if __name__ == "__main__":
+    connect_mongodb()
