ae2d6263e51d717dfe9dd9db02bc8a25669c2f13
software groups <admin@sfgroups.com>
2023-01-08 21:57:52 -0500

    added shopping car exception

--- src/python_tools/pytest_samples/load_data.py
+++ src/python_tools/pytest_samples/load_data.py
@@ -4,3 +4,12 @@ from python_tools.pytest_samples.slow import DataSet
 def slow_load():
     dataset = DataSet()
     return dataset.load_data()
+
+
+def slow_load_ex():
+    try:
+        dataset = DataSet()
+        return dataset.load_data()
+    except Exception as e:
+        raise e
+
--- tests/pytest_samples/test_load_data.py
+++ tests/pytest_samples/test_load_data.py
@@ -3,25 +3,25 @@ from unittest.mock import patch
 import pytest
 
 from python_tools.pytest_samples.load_data import slow_load
-#
-#
-# def test_slow_load():
-#     assert "Data" in slow_load()
-#
-#
-# def test_slow_load(mocker):
-#     assert "Data" in slow_load()
-#
-#     expected = 'mock load'
-#
-#     def mock_load(self):
-#         return 'mock load'
-#
-#     mocker.patch("python_tools.pytest_samples.load_data.DataSet.load_data", mock_load)
-#     actual = slow_load()
-#     assert actual == expected
-#
-#
+
+
+def test_slow_load():
+    assert "Data" in slow_load()
+
+
+def test_slow_load(mocker):
+    assert "Data" in slow_load()
+
+    expected = 'mock load'
+
+    def mock_load(self):
+        return 'mock load'
+
+    mocker.patch("python_tools.pytest_samples.load_data.DataSet.load_data", mock_load)
+    actual = slow_load()
+    assert actual == expected
+
+
 def test_slow_load_01(mocker):
     with mocker.patch("python_tools.pytest_samples.load_data.DataSet.load_data", side_effect=Exception('URLError')):
         with pytest.raises(Exception) as excinfo:
