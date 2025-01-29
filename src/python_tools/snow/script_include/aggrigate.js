21698bb74fcc9af764cf7ed5732402335b45f256
software groups <admin@sfgroups.com>
2025-01-28 22:00:53 -0500

    added script logic to get the application version number

--- src/python_tools/snow/script_include/aggrigate.js
+++ src/python_tools/snow/script_include/aggrigate.js
@@ -1,19 +1,45 @@
-try {
-    var agg = new GlideAggregate('cmdb_software_product_model');
-    var queryString = 'application_model=35989b9183a69210bef196a6feaad3b6';
-    var isValidQuery = agg.isValidEncodedQuery(queryString);
-    if (isValidQuery) {
-        agg.addEncodedQuery(queryString);
-        agg.addAggregate('MAX', 'version');
-        agg.setGroup(false);
-        agg.query();
+function getSysIdByAppName(appName) {
+    if (!appName || typeof appName !== 'string') {
+        return 'Invalid application name';
+    }
+    var trimmedAppName = appName.trim(); // Trim leading and trailing spaces
 
-        while (agg.next()) {
-            var maxValue = agg.getAggregate('MAX', 'version');
-            gs.info('Maximum version: ' + maxValue);
-        }
+    var gr = new GlideRecord('cmdb_application_product_model');
+    gr.addQuery('name', appName);
+    gr.query();
+
+    if (gr.next()) {
+        return gr.getValue('sys_id');
     } else {
-        throw "Encoded query is invalid. Please check and update the script";
+        return 'No matching record found';
+    }
+}
+
+
+try {
+    var appName = 'Clear Path';
+    var sysId = getSysIdByAppName(appName);
+    gs.info('Sys ID: ' + sysId);
+    var sysIdRegex = /^[0-9a-f]{32}$/;
+    if (!sysIdRegex.test(sysId)) {
+        gs.error("invalid sysId");
+    } else {
+        var agg = new GlideAggregate('cmdb_software_product_model');
+        var queryString = 'application_model=' + sysId;
+        var isValidQuery = agg.isValidEncodedQuery(queryString);
+        if (isValidQuery) {
+            agg.addEncodedQuery(queryString);
+            agg.addAggregate('MAX', 'version');
+            agg.setGroup(false);
+            agg.query();
+
+            while (agg.next()) {
+                var maxValue = agg.getAggregate('MAX', 'version');
+                gs.info('Maximum version: ' + maxValue);
+            }
+        } else {
+            throw "Encoded query is invalid. Please check and update the script";
+        }
     }
 } catch (ex) {
     var message = ex + '';
